c58ad540cb4f23668b7951cd346047acfdf59dde
imtheknown <theknownofficial@gmail.com>
2020-10-04 15:20:06 +0545

    Addition of routes and model for users

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const mongooseConfig = {
 mongoose.connect(process.env.MONGO_URI,mongooseConfig,()=>{
     console.log('Connected To DB');
 });
+
+app.use(express.json());
+
 //Importing the router 
 const authRoutes = require('./routes/auth');
 
 //Using the auth middleware
 app.use('/api/v1/user',authRoutes)
+
 
 
 const PORT = process.env.PORT || 5000;
