cacf2ba2f40965e54b36ddf8af413dde967c9cbf
imtheknown <theknownofficial@gmail.com>
2020-10-04 17:49:19 +0545

    password validation added bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/user');
 const { registerValidation } = require('../middlewares/validation');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req,res)=>{
 
@@ -11,18 +12,22 @@ router.post('/register', async (req,res)=>{
     }
 
     //Check if the user exists
-    const emailExists = User.findOne({
+    const emailExists = await User.findOne({
         email: req.body.email
     });
     if(emailExists){
-        return res.status(400).send('Email Already Exists')
+        return res.status(400).send('Email Already Exists');
     }
-    
+
+    //Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create an user from schema
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
     });
     
     try{
